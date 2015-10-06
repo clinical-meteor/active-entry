8233a5ca4256fa18c9fd41b4bcb1af86d36e0ac7
Abigail Watson <awatson1978@gmail.com>
2015-10-06 14:38:43 -0700

    bump to 0.2.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-entry',
-  version: '0.2.1',
+  version: '0.2.2',
   // Brief, one-line summary of the package.
   summary: 'SignIn, SignUp, and ForgotPassword pages for Clinical Framework. ',
   // URL to the Git repository containing the source code for this package.
@@ -13,6 +13,8 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.0.4');
   //api.addFiles('clinical-entry-pages.js');
+
+  api.use('photonic:glass-ui');
 
   api.use([
     'templating',
