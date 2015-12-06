ef3e2a77bb265ede6e8a3f6b38cce44753348d64
Abigail Watson <awatson1978@gmail.com>
2015-12-06 10:56:44 -0800

    general checkin

--- tests/gagarin/walkthroughs/basic.js
+++ tests/gagarin/walkthroughs/basic.js
@@ -1,6 +1,6 @@
 // var nightwatch = require('nightwatch');
 
-describe('Sign In Page', function() {
+describe('Entry', function() {
 
   before(function () {
     return client.execute(function () {
@@ -8,14 +8,66 @@ describe('Sign In Page', function() {
     });
   });
 
-  it("should work on tablet", function () {
+  it("new user should be able to register on desktop", function () {
 
   });
-  it("should work on phone", function () {
+  it("company logo should display on sign-in page", function () {
 
   });
-  it("should work on desktop", function () {
+  it("error messages should be empty by default", function () {
 
   });
+  it("user should be able to request to create new account", function () {
+
+  });
+  it("guest should be notified if username already exists", function () {
+
+  });
+  it("guest should be notified if passwords do not match", function () {
+
+  });
+  it("guest should be notified if email is not correctly formatted", function () {
+
+  });
+  it("when new user fills out form and registers, new user should get created", function () {
+
+  });
+  it("when user signs in with username and password, should redirect to home page", function () {
+
+  });
+  it("user should be able to request reset password email", function () {
+
+  });
+  it("existing user should be able to sign in on desktop", function () {
+
+  });
+  it("existing user should be able to sign in on tablet", function () {
+
+  });
+  it("existing user should be able to sign in on phone", function () {
+
+  });
+  it("new user should be able to register on tablet", function () {
+
+  });
+  it("user object should return first name", function () {
+
+  });
+  it("user object should return last name", function () {
+
+  });
+  it("config should be able to change company logo", function () {
+
+  });
+  it("config should be able to change entry message text", function () {
+
+  });
+  it("newly created user record should have role", function () {
+
+  });
+  it("ewly created user record should have full name, preferred name, and family name", function () {
+
+  });
+
 });
 ````
