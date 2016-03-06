696dd6a9475726e7bd679ad07f37e7d8477cb497
Abigail Watson <awatson1978@gmail.com>
2016-03-05 18:25:09 -0800

    removed meteorLogin

--- components/entrySignIn/.tests/actions/meteorLogin.js
+++
@@ -1,8 +0,0 @@
-exports.command = function (email, password) {
-  this
-    .execute(function () {
-      return Meteor.loginWithPassword(email, password);
-    }).pause(1000);
-
-  return this;
-};
